26bcb2a4f6e36f3af6a5ebae6889a508b020a35b
Muhammad Haitsam <54610524+mhaitsam18@users.noreply.github.com>
2023-05-26 02:06:30 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [AuthController::class, 'index'])->name('auth.index');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home.index');
