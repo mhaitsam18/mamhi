855cf9acfe20cfe520188201e13ec8e012e35cc4
Muhammad Haitsam <haitsam03@gmail.com>
2023-06-21 01:02:43 +0700

    Update mamhi.sql

--- mamhi.sql
+++ mamhi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2023 at 11:17 PM
+-- Generation Time: Jun 20, 2023 at 08:02 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -60,12 +60,91 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `jadwal` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `psikolog_id` bigint(20) UNSIGNED DEFAULT NULL,
   `hari` varchar(255) DEFAULT NULL,
-  `jam` time DEFAULT NULL,
+  `jam_mulai` time DEFAULT NULL,
+  `jam_selesai` time DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `jadwal`
+--
+
+INSERT INTO `jadwal` (`id`, `psikolog_id`, `hari`, `jam_mulai`, `jam_selesai`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 'Selasa', '13:30:00', '14:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(2, 1, 'Selasa', '15:00:00', '14:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(3, 1, 'Kamis', '13:30:00', '14:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(4, 1, 'Kamis', '15:00:00', '14:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(5, 1, 'Sabtu', '08:00:00', '09:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(6, 1, 'Sabtu', '09:20:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(7, 1, 'Sabtu', '13:30:00', '14:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(8, 1, 'Sabtu', '15:00:00', '16:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(9, 2, 'Rabu', '10:00:00', '10:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(10, 2, 'Rabu', '11:00:00', '11:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(11, 2, 'Jumat', '09:00:00', '09:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(12, 2, 'Jumat', '10:00:00', '10:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(13, 2, 'Jumat', '14:00:00', '14:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(14, 2, 'Jumat', '15:00:00', '16:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(15, 2, 'Sabtu', '10:00:00', '10:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(16, 2, 'Sabtu', '11:00:00', '11:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(17, 3, 'Selasa', '08:00:00', '08:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(18, 3, 'Selasa', '11:00:00', '11:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(19, 3, 'Kamis', '11:00:00', '11:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(20, 3, 'Kamis', '15:00:00', '15:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(21, 3, 'Jumat', '10:00:00', '10:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(22, 3, 'Jumat', '11:00:00', '11:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(23, 3, 'Jumat', '13:30:00', '14:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(24, 3, 'Jumat', '15:00:00', '16:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(25, 4, 'Kamis', '09:30:00', '10:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(26, 4, 'Kamis', '11:00:00', '12:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(27, 4, 'Kamis', '15:00:00', '16:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(28, 4, 'Jumat', '08:00:00', '09:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(29, 4, 'Jumat', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(30, 4, 'Jumat', '15:00:00', '16:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(31, 4, 'Sabtu', '08:00:00', '09:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(32, 4, 'Sabtu', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(33, 5, 'Selasa', '10:00:00', '10:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(34, 5, 'Selasa', '13:00:00', '13:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(35, 5, 'Selasa', '14:00:00', '14:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(36, 5, 'Selasa', '15:00:00', '15:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(37, 5, 'Rabu', '09:00:00', '09:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(38, 5, 'Rabu', '10:00:00', '10:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(39, 5, 'Rabu', '13:00:00', '13:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(40, 5, 'Rabu', '14:00:00', '14:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(41, 5, 'Kamis', '15:00:00', '15:55:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(42, 6, 'Selasa', '08:00:00', '09:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(43, 6, 'Selasa', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(44, 6, 'Rabu', '08:00:00', '09:20:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(45, 6, 'Rabu', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(46, 7, 'Selasa', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(47, 7, 'Selasa', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(48, 7, 'Selasa', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(49, 7, 'Selasa', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(50, 7, 'Selasa', '09:30:00', '10:50:00', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(51, 7, 'Kamis', '09:30:00', '10:50:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(52, 7, 'Kamis', '09:30:00', '10:50:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(53, 7, 'Kamis', '09:30:00', '10:50:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(54, 7, 'Kamis', '09:30:00', '10:50:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(55, 7, 'Kamis', '09:30:00', '10:50:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(56, 8, 'Kamis', '09:00:00', '09:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(57, 8, 'Kamis', '10:00:00', '10:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(58, 8, 'Kamis', '13:00:00', '13:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(59, 8, 'Kamis', '14:00:00', '14:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(60, 8, 'Jumat', '09:00:00', '09:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(61, 8, 'Jumat', '11:00:00', '11:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(62, 8, 'Jumat', '13:00:00', '13:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(63, 8, 'Jumat', '14:00:00', '14:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(64, 9, 'Sabtu', '09:30:00', '10:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(65, 9, 'Sabtu', '11:00:00', '12:20:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(66, 9, 'Sabtu', '13:30:00', '14:50:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(67, 9, 'Sabtu', '15:00:00', '16:20:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(68, 10, 'Sabtu', '08:00:00', '09:20:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(69, 10, 'Sabtu', '09:30:00', '10:55:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(70, 10, 'Sabtu', '13:30:00', '14:50:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL),
+(71, 10, 'Sabtu', '15:00:00', '16:20:00', '2023-06-20 11:00:38', '2023-06-20 11:00:38', NULL);
 
 -- --------------------------------------------------------
 
@@ -121,7 +200,7 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`id`, `user_id`, `pekerjaan`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 2, NULL, '2023-06-19 20:35:16', '2023-06-19 20:35:24', NULL);
+(1, 2, 'Mahasiswa', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL);
 
 -- --------------------------------------------------------
 
@@ -231,6 +310,22 @@ CREATE TABLE `psikolog` (
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `psikolog`
+--
+
+INSERT INTO `psikolog` (`id`, `user_id`, `jenis_keahlian`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 3, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(2, 4, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(3, 5, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(4, 6, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(5, 7, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(6, 8, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(7, 9, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(8, 10, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(9, 11, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(10, 12, 'Psikolog', '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -281,8 +376,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `tanggal_lahir`, `no_hp`, `jenis_kelamin`, `alamat`, `email_verified_at`, `password`, `role`, `foto`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Zitha', 'zitha@gmail.com', 'zitha', '2023-06-19', '0123456789', 'Perempuan', 'Bandung', NULL, '$2y$10$qrIsnAlJp00GDgsPi/q0peU/chupibQT9U7QhabDzmBycIzYq0ZYu', 'admin', 'foto-profil/OxImynHEbiDCx9B34DVzHPHRgm0m5pmEEmrLbOOx.png', NULL, '2023-06-19 09:39:59', '2023-06-19 09:39:59', NULL),
-(2, 'Derisa', 'derisa@gmail.com', 'derisa', '2023-06-20', '0812345678', 'Perempuan', 'Bandung', NULL, '$2y$10$CIGXB6Q9cssEOKZ2VSURmO894npXcLb40lCbHu0jtWVQZFT9yycPK', 'member', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-19 13:32:50', '2023-06-19 13:32:50', NULL);
+(1, 'Zitha', 'zitha@gmail.com', 'zitha', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$v2l2TzAzxXRfksSxfdnYaOB.iM.aqeavy6ssQmFgrq.o5X7ZVCedW', 'admin', 'foto-profil/OxImynHEbiDCx9B34DVzHPHRgm0m5pmEEmrLbOOx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(2, 'Derisa', 'derisa@gmail.com', 'derisa', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$NUOYRRkKPC9x2ElxYvkYHud4ksRgrXTT40CK2Zlvd0tNz5/EvfFJu', 'member', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(3, 'Inheke', 'inheke@gmail.com', 'inheke', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$MlYoWNyosB/9pTFo7mzelu./D41YdhRYXBJU14tII4QX1atvx/eZ.', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(4, 'K Dyka', 'kdyka@gmail.com', 'kdyka', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$GBRVYli0XggfUlHOQxfhWeimjDYSBV.bqlDha9Cuiz5qezqUYYK8O', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(5, 'Bunis', 'bunis@gmail.com', 'bunis', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$ec6n228nojSHkwdT3SQ/yeavZtbiZu9tAWKOEtglNCM49b5XS1gdO', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(6, 'Yui', 'yui@gmail.com', 'yui', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$reeaGHtSlyKdkfVb2RwBcuTky6pUKap4dONR9CCoHbUDTD0q5FEy.', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(7, 'Mia', 'mia@gmail.com', 'mia', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$0NrW31yVX8ldSdYxd5xJze0QIk7MSu3IdZoqeEp4m1cTiFM1lmZ2.', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(8, 'Prast', 'prast@gmail.com', 'prast', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$UDP.IqWphzj.yvoAfbynzuhlKoxobNN3l3TP4.L5PJExFy439.Qru', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(9, 'Linda', 'linda@gmail.com', 'linda', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$4YvxMS/jjdMZblMyjBZzsuHYScmlzJWrLRtoE6cxwtOIfdUROW3Lq', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(10, 'Ica', 'ica@gmail.com', 'ica', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$ceKKGvQDQEZYwfziZQ8Ije6mX2t1svJOtkjaAS4VZTywIduwdedn2', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(11, 'T Dyah', 'tdyah@gmail.com', 'tdyah', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$DV/P9h.lPh4VfeNBI1WPaO9QDKGcm.b6ETq5ugPjE1NOrbxezCTum', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL),
+(12, 'Wulan', 'wulan@gmail.com', 'wulan', '2022-02-18', '+62 813-8087-6176', 'Perempuan', 'Bandung', NULL, '$2y$10$TLE99pRZc2SMQco2t8CIOeui0k3D4krdU5C7KV.bxxRU7U4hMK3Mi', 'psikolog', 'foto-profil/DgGChffO4u3V7dJ79MTakaEBRvKif6WbFljemwnx.png', NULL, '2023-06-20 11:00:37', '2023-06-20 11:00:37', NULL);
 
 --
 -- Indexes for dumped tables
@@ -306,7 +411,8 @@ ALTER TABLE `failed_jobs`
 -- Indexes for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jadwal_psikolog_id_foreign` (`psikolog_id`);
 
 --
 -- Indexes for table `komponen_nilai`
@@ -409,7 +515,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- AUTO_INCREMENT for table `komponen_nilai`
@@ -457,7 +563,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `psikolog`
 --
 ALTER TABLE `psikolog`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `psikotes`
@@ -469,7 +575,7 @@ ALTER TABLE `psikotes`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -480,6 +586,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `diagnosis`
   ADD CONSTRAINT `diagnosis_konsultasi_id_foreign` FOREIGN KEY (`konsultasi_id`) REFERENCES `konsultasi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `jadwal_psikolog_id_foreign` FOREIGN KEY (`psikolog_id`) REFERENCES `psikolog` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `konsultasi`
