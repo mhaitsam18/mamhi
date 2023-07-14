bda694268f050a4cb45b8571ceb1831bee785571
Muhammad Haitsam <54610524+mhaitsam18@users.noreply.github.com>
2023-07-15 03:06:48 +0700

    controller psikotes dan konsultasi

--- app/Http/Controllers/AdminKonsultasiController.php
+++ app/Http/Controllers/AdminKonsultasiController.php
@@ -2,12 +2,131 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jadwal;
+use App\Models\Konsultasi;
+use App\Models\Member;
+use App\Models\Psikolog;
 use Illuminate\Http\Request;
 
 class AdminKonsultasiController extends Controller
 {
 
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
+        return view('admin.konsultasi.index', [
+            'title' => 'List konsultasi',
+            'page' => 'konsultasi',
+            'konsultasis' => Konsultasi::all(),
+            'members' => Member::all(),
+            'psikologs' => Psikolog::all(),
+            'jadwals' => Jadwal::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.konsultasi.create', [
+            'title' => 'Buat konsultasi',
+            'page' => 'konsultasi',
+            'konsultasis' => Konsultasi::all(),
+            'members' => Member::all(),
+            'psikologs' => Psikolog::all(),
+            'jadwals' => Jadwal::all(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'member_id' => 'required',
+            'psikolog_id' => 'required',
+            'keluhan' => 'required',
+            'tanggal_konsultasi' => 'required',
+            'jadwal_id' => 'required',
+        ]);
+
+        Konsultasi::create([
+            'member_id' => $request->member_id,
+            'psikolog_id' => $request->psikolog_id,
+            'keluhan' => $request->keluhan,
+            'booked_at' => date('Y-m-d H:i:s'),
+            'tanggal_konsultasi' => $request->tanggal_konsultasi,
+            'jadwal_id' => $request->jadwal_id,
+            'status' => $request->status
+        ]);
+
+        return redirect('/admin/konsultasi')->with('success', 'Data konsultasi ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Konsultasi $konsultasi)
+    {
+        return view('admin.konsultasi.show', [
+            'title' => 'Detail konsultasi',
+            'page' => 'konsultasi',
+            'konsultasi' => $konsultasi,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Konsultasi $konsultasi)
+    {
+        return view('admin.konsultasi.edit', [
+            'title' => 'Detail konsultasi',
+            'page' => 'konsultasi',
+            'konsultasi' => $konsultasi,
+            'members' => Member::all(),
+            'psikologs' => Psikolog::all(),
+            'jadwals' => Jadwal::all(),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Konsultasi $konsultasi)
+    {
+        $request->validate([
+            'member_id' => 'required',
+            'psikolog_id' => 'required',
+            'keluhan' => 'required',
+            'tanggal_konsultasi' => 'required',
+            'jadwal_id' => 'required',
+            'status' => 'required',
+        ]);
+
+        
+        $konsultasi->update([
+            'member_id' => $request->member_id,
+            'psikolog_id' => $request->psikolog_id,
+            'keluhan' => $request->keluhan,
+            'tanggal_konsultasi' => $request->tanggal_konsultasi,
+            'jadwal_id' => $request->jadwal_id,
+            'status' => $request->status
+        ]);
+
+        return redirect('/admin/konsultasi')->with('success', 'Data konsultasi diperbarui');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(konsultasi $konsultasi)
+    {
+        $konsultasi->delete();
+        return redirect('/admin/konsultasi')->with('success', 'Data konsultasi dihapus');
     }
 }
--- app/Http/Controllers/AdminPsikotesController.php
+++ app/Http/Controllers/AdminPsikotesController.php
@@ -2,12 +2,138 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jadwal;
+use App\Models\Member;
+use App\Models\Psikolog;
+use App\Models\Psikotes;
 use Illuminate\Http\Request;
 
 class AdminPsikotesController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        
+        return view('admin.psikotes.index', [
+            'title' => 'List psikotes',
+            'page' => 'psikotes',
+            'psikotess' => Psikotes::all(),
+            'members' => Member::all(),
+            'psikologs' => Psikolog::all(),
+            'jadwals' => Jadwal::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.psikotes.create', [
+            'title' => 'Buat psikotes',
+            'page' => 'psikotes',
+            'psikotess' => Psikotes::all(),
+            'members' => Member::all(),
+            'psikologs' => Psikolog::all(),
+            'jadwals' => Jadwal::all(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'member_id' => 'required',
+            'psikolog_id' => 'required',
+            'nomor_peserta' => 'unique:psikotes',
+            'tanggal_psikotes' => 'required',
+            'jenis_psikotes' => 'required',
+            'kebutuhan' => 'required',
+            'jadwal_id' => 'required',
+        ]);
+
+        psikotes::create([
+            'member_id' => $request->member_id,
+            'psikolog_id' => $request->psikolog_id,
+            'nomor_peserta' => $request->nomor_peserta,
+            'booked_at' => date('Y-m-d H:i:s'),
+            'tanggal_psikotes' => $request->tanggal_psikotes,
+            'jenis_psikotes' => $request->jenis_psikotes,
+            'kebutuhan' => $request->kebutuhan,
+            'jadwal_id' => $request->jadwal_id,
+            'status' => $request->status
+        ]);
+
+        return redirect('/admin/psikotes')->with('success', 'Data psikotes ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(psikotes $psikotes)
+    {
+        return view('admin.psikotes.show', [
+            'title' => 'Detail psikotes',
+            'page' => 'psikotes',
+            'psikotes' => $psikotes,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(psikotes $psikotes)
+    {
+        return view('admin.psikotes.edit', [
+            'title' => 'Detail psikotes',
+            'page' => 'psikotes',
+            'psikotes' => $psikotes,
+            'members' => Member::all(),
+            'psikologs' => Psikolog::all(),
+            'jadwals' => Jadwal::all(),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, psikotes $psikotes)
+    {
+        $request->validate([
+            'member_id' => 'required',
+            'psikolog_id' => 'required',
+            'nomor_peserta' => 'unique:psikotes',
+            'tanggal_psikotes' => 'required',
+            'jenis_psikotes' => 'required',
+            'kebutuhan' => 'required',
+            'jadwal_id' => 'required',
+            'status' => 'required',
+        ]);
+
+
+        $psikotes->update([
+            'member_id' => $request->member_id,
+            'psikolog_id' => $request->psikolog_id,
+            'nomor_peserta' => $request->nomor_peserta,
+            'tanggal_psikotes' => $request->tanggal_psikotes,
+            'jenis_psikotes' => $request->jenis_psikotes,
+            'kebutuhan' => $request->kebutuhan,
+            'jadwal_id' => $request->jadwal_id,
+            'status' => $request->status
+        ]);
+
+        return redirect('/admin/psikotes')->with('success', 'Data psikotes diperbarui');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(psikotes $psikotes)
+    {
+        $psikotes->delete();
+        return redirect('/admin/psikotes')->with('success', 'Data psikotes dihapus');
     }
 }
