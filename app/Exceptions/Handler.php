8f3c7af6aab30b9233bc2b1f6083d18263de2efa
Muhammad Haitsam <54610524+mhaitsam18@users.noreply.github.com>
2023-09-08 10:30:53 +0700

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -59,13 +59,14 @@ class Handler extends ExceptionHandler
         }
 
         // Penanganan pengecualian lainnya
-        // if ($exception instanceof \Exception) {
-        //     return response()->view('errors.index', [
-        //         'title' => 'Terjadi Kesalahan',
-        //         'message' => 'Terjadi kesalahan dalam sistem',
-        //         'code' => '500',
-        //     ], 500);
-        // }
+        if ($exception instanceof \Exception && !($exception instanceof \Illuminate\Validation\ValidationException)) {
+            return response()->view('errors.index', [
+                'title' => 'Terjadi Kesalahan',
+                'message' => 'Terjadi kesalahan dalam sistem',
+                'code' => '500',
+            ], 500);
+        }
+
 
         return parent::render($request, $exception);
     }
